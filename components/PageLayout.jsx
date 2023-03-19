2dc1f7655bb84211f834042e400b49a8dc7fb9e1
diy-pwa <122644285+diy-pwa@users.noreply.github.com>
2023-03-18 22:08:25 -0400

    add a css library and a font example

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'w3-css/w3.css';
+import '@fontsource/recursive';
 import './PageLayout.css';
 
 export function PageLayout({ children }) {
--- renderer/_default.page.server.jsx
+++ renderer/_default.page.server.jsx
@@ -23,6 +23,8 @@ async function render(pageContext) {
     <html>
       <head>
         <title>${documentProps.title}</title>
+        <meta name="description" content="${documentProps.description}" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
         <div id="page-view">${stream}</div>
