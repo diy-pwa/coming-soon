8b286237d476c927dd239e345ef1f646b504c5f9
diy-pwa <122644285+diy-pwa@users.noreply.github.com>
2023-03-19 22:25:55 -0400

    1 binaries uploaded

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default {
     mdx(),
     ssr({
       prerender: true,
+      includeAssetsImportedByServer: true
     }),
   ],
   // We manually add a list of dependencies to be pre-bundled, in order to avoid a page reload at dev start which breaks vite-plugin-ssr's CI
