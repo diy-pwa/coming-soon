ccfe15a64e6f9a53b2e17f2b6c1598aed911d643
Rich Hildred <rhildred@gmail.com>
2023-03-22 13:53:51 -0400

    change import from vite-plugin-ssr to vite-plugin-ssr-server

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,5 @@ export default {
       prerender: true,
       includeAssetsImportedByServer: true,
     }),
-  ],
-  // We manually add a list of dependencies to be pre-bundled, in order to avoid a page reload at dev start which breaks vite-plugin-ssr's CI
-  // (The 'react/jsx-runtime' entry is not needed in Vite 3 anymore.)
-  optimizeDeps: { include: ['cross-fetch', 'react/jsx-runtime'] },
+  ]
 };
