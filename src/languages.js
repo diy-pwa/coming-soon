99a47d1331bd92b0d27e56da19a58fa7ffa64844
Rich Hildred <rhildred@wlu.ca>
2023-05-26 20:26:52 -0400

    add direction to languages

--- src/languages.js
+++ src/languages.js
@@ -6,15 +6,15 @@ export default ()=>{
     const languages = config['io_github_diy-pwa_languages'];
     let sLang = null;
     for(let n= 0; n < languages.length; n++){
-        const re = new RegExp(`^${languages[n]}`);
+        const re = new RegExp(`^${languages[n].language}`);
         if(sAccept.match(re)){
             // there is a language that is in navigator.language
-            sLang = languages[n];
+            sLang = languages[n].language;
             break;
         }
     }
     if(!sLang){
-        sLang =  languages[0];
+        sLang =  languages[0].language;
     }
     return sLang;
 }
